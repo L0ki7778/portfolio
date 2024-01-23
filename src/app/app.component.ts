caec0c30dff3bcd2fb8d52c5bdb79c4abed2437c
Rene Heller <128136936+L0ki7778@users.noreply.github.com>
2024-01-23 16:59:39 +0100

    start animation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { IntroComponent } from './intro/intro.component';
+import { MainPageComponent } from './main-page/main-page.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet,IntroComponent],
+  imports: [CommonModule, RouterOutlet,IntroComponent,MainPageComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
