e825422c09c04c6a8c88141003891b3e1f91ae15
Rene Heller <128136936+L0ki7778@users.noreply.github.com>
2024-01-25 20:40:19 +0100

    more animation and responsiveness

--- src/app/main-page/main-page-start/selfie/selfie.component.ts
+++ src/app/main-page/main-page-start/selfie/selfie.component.ts
@@ -3,11 +3,12 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatDividerModule } from '@angular/material/divider';
 import { MatButtonModule } from '@angular/material/button';
 import { ButtonComponent } from '../../button/button.component';
+import { ScrollDownComponent } from '../../../scroll-down/scroll-down.component';
 
 @Component({
   selector: 'app-selfie',
   standalone: true,
-  imports: [MatButtonModule, MatDividerModule, MatIconModule, ButtonComponent ],
+  imports: [MatButtonModule, MatDividerModule, MatIconModule, ButtonComponent,ScrollDownComponent ],
   templateUrl: './selfie.component.html',
   styleUrl: './selfie.component.scss'
 })
